91f423e87356ce2e05e7d106d6ba28ad77f8b598
Vlad Krylov <vladd7589@gmail.com>
2024-10-04 23:22:54 +0300

    Create getSymbol()/getNumber()

--- app/src/main/java/com/sky/calculator/ViewModel.kt
+++ app/src/main/java/com/sky/calculator/ViewModel.kt
@@ -1,7 +1,27 @@
 package com.sky.calculator
 
+import android.view.View
+import android.widget.Button
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ViewModel : ViewModel() {
+    private var _currentExpression = ""
+    val currentExpression
+        get() = _currentExpression
+    var getAns = false
 
+    fun getSymbol(view: View) {
+        getAns = false
+        val btn = view as Button
+        _currentExpression += btn.text.toString()
+    }
+    fun getNumber(view: View) {
+        if(getAns) {
+            _currentExpression = ""
+            getAns = false
+        }
+        val btn = view as Button
+        _currentExpression += btn.text.toString()
+    }
 }
