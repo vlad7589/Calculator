6329a7f0de8576621ae4beb7f0213f9f2f090279
Vlad Krylov <vladd7589@gmail.com>
2024-10-08 01:22:10 +0300

    Create plusMinus()

--- app/src/main/java/com/sky/calculator/ViewModel.kt
+++ app/src/main/java/com/sky/calculator/ViewModel.kt
@@ -12,13 +12,16 @@ class ViewModel : ViewModel() {
         get() = _currentExpression
     var getAns = false
     var pressPercent = false
+    var plusMinusOn = false
 
     var translateText = MutableLiveData<String>("")
     fun getSymbol(view: View) {
         getAns = false
+        plusMinusOn = false
         val btn = view as Button
         when(btn.text.toString()) {
             "%" -> percent()
+            "+/-" -> plusMinusBtn()
             else -> {
                 _currentExpression += btn.text.toString()
                 translateText.value += btn.text.toString()
@@ -39,11 +42,16 @@ class ViewModel : ViewModel() {
     }
 
     fun equals() {
-        val exp = ExpressionBuilder(currentExpression).build()
+        val parenthesisCount = currentExpression.count { it == '(' } - currentExpression.count { it == ')' }
+        val exp = ExpressionBuilder(currentExpression + ')'.toString().repeat(parenthesisCount)).build()
         val res = exp.evaluate()
         getAns = true
+        _currentExpression = res.toString()
         if(res.toInt() - res == 0.0) translateText.value = res.toInt().toString()
         else translateText.value = res.toString()
+        pressPercent = false
+        if(res < 0) plusMinusOn = true
+        else plusMinusOn = false
     }
 
     fun percent(){
@@ -52,8 +60,40 @@ class ViewModel : ViewModel() {
         pressPercent = true
     }
 
+    fun plusMinusBtn(){
+        val indexExp = _currentExpression.indexOfLast { when(it) {
+            '+', '-', '*', '/' -> true
+            else -> false
+        } } + 1
+        val indexTrl = translateText.value?.indexOfLast {
+            when(it) {
+                '+', '-', '*', '/' -> true
+                else -> false
+            } } as Int + 1
+        if(!plusMinusOn) {
+            translateText.value = translateText.value?.subSequence(0, indexTrl).toString() +
+                    "(-" +
+                    translateText.value?.subSequence(indexTrl, translateText.value.toString().length).toString()
+            _currentExpression = _currentExpression.subSequence(0, indexExp).toString() +
+                    "(-" +
+                    _currentExpression.subSequence(indexExp, _currentExpression.length).toString()
+            plusMinusOn = true
+        } else if(getAns && _currentExpression.toDouble() < 0) {
+            translateText.value = translateText.value.toString().removeRange(0,1)
+            _currentExpression = _currentExpression.removeRange(0,1)
+            plusMinusOn = false
+        } else {
+            val index = translateText.value.toString().lastIndexOf("(-")
+            translateText.value = translateText.value.toString().removeRange(index,index+2)
+            _currentExpression = _currentExpression.removeRange(index, index+2)
+            plusMinusOn = false
+        }
+        getAns = false
+    }
+
     fun clearText() {
         translateText.value = ""
         _currentExpression = ""
+        plusMinusOn = false
     }
 }
